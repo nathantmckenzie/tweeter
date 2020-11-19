86fe7dacdcd7a474a8f163202a26a347b4e82228
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-18 16:54:59 -0800

    Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,57 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready (() => {
 
+// Fake data taken from initial-tweets.json
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  let result = {};
+  for (let tweet of tweets) {
+      result = createTweetElement(tweet);
+      $('#tweets-container').append(result);
+  }
+  return result; 
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = $(`
+  <br>
+  <article class="tweet">
+  <header class="tweet-header">
+  <img class="tweet-avatar" src="https://i.imgur.com/73hZDYK.png">
+  <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nisl in .</h3>
+  <p>10 days ago</p>
+  </header>
+   </article>
+  `);
+  return $tweet;
+}
+
+renderTweets(data);
+
+});
