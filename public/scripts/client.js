b587bf6f89fcd890a44a2648d7fe872e33665975
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-19 10:00:32 -0800

    Fetching tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -61,6 +36,16 @@ const tweetSubmit = function() {
     })
 }
 
+const loadTweets = function() {
+   $.ajax({
+     type: "GET",
+     url: '/tweets',
+     dataType: 'json'
+   }).then(function (data) {
+     renderTweets(data);
+   })
+
+}
 
 
 renderTweets(data);
