90ecbaba81ac822920c85d11ca3438719c139264
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-20 17:19:34 -0800

    Reformatting code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,77 +1,89 @@
 $(document).ready(function() {
 
+const $errorShort = $('#error-short');
+const $errorLong = $('#error-long');
 const $form = $('.tweet-submission');
 const $textArea = $('#tweet-text');
 const $tweetsContainer = $('#tweets-container');
+const $newTweet = $('.new-tweet');
+const $counter = $('.counter');
+const newTweet = function() {
+  $newTweet.on('click', function() {
+    $textArea.focus();
+  })
+}
 
-const escape =  function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
+newTweet();
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function(tweet) {
   const safeHTML = `${escape(tweet.content.text)}`;
-  let tweetOutput = $(`
-  <section id="tweet-container">
-  <br>
+  let $tweetOutput = $(`
   <article class="tweet">
   <header class="tweet-header">
-  <img class="tweet-avatar" src="${tweet.user.avatars}">
-  <span class="tweet-username" src="${tweet.user.name}">
-  <span class="tweet-handle" src="${tweet.user.handle}">
-  </header>
-  <h3>
-  ${safeHTML};
-  </h3>
-  <p>10 days ago</p>
-  </header>
-   </article>
-   </section>
+    <img class="tweet-avatar" src="${tweet.user.avatars}" />
+    <span class="tweet-username">${tweet.user.name}</span>
+    <span class="tweet-handle">${tweet.user.handle}</span>
+    </header>
+  <h3>${safeHTML}</h3>
+  <footer class="tweet-footer">
+    <h4 class="tweet-date">10 days ago</h4>
+    <div class="tweet-icons">
+      <i class="fa fa-flag"></i>
+      <i class="fa fa-heart"></i>
+      <i class="fa fa-retweet"></i>
+    </div>
+    </footer>
+  </article> 
   `);
-  return tweetOutput;
+  return $tweetOutput;
 }
 
 const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    //let result = {};
-    for (let tweet of tweets) {
-        $tweetsContainer.prepend(createTweetElement(tweet));
-    }
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    $tweetsContainer.prepend(createTweetElement(tweet));
   }
-
- 
-const loadTweets = function() {
-   $.ajax({
-     url: '/tweets',
-     method: "GET",
-     dataType: "json",
-     success: (tweets) => { renderTweets(tweets) }
-   })
 }
 
-$form.on('submit', function (event) {
-    event.preventDefault();
-    if ($textArea.val() === '') {
-        alert("Missing Input");
-    } else if ($textArea.val().length > 140) {
-        alert("Character Limit Exceeded");
-    } else {
-      const serializedData = $(this).serialize();
-       $.post('/tweets', serializedData)
-       .then(() => {
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    method: "GET",
+    dataType: "json",
+    success: (tweets) => {
+      renderTweets(tweets)
+    }
+  })
+}
+loadTweets();
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+  $errorShort.slideUp(1000);
+  $errorLong.slideUp(1000);
+  if ($textArea.val().length <= 0) {
+    $errorShort.slideDown(1000);
+    /*$errorMessage.slideDown(1000);*/
+  } else if ($textArea.val().length > 140) {
+    $errorLong.slideDown(1000);
+  } else {
+    const serializedData = $(this).serialize();
+    $.post('/tweets', serializedData)
+      .then((data) => {
         $form.trigger('reset');
+        $counter.val(140);
         loadTweets();
         console.log("Sucess: ", data);
-       }).catch(function (data) {
-         console.log("Error: ", data);
-       });
-    }
-    })
-
-
-
+      }).catch(function(data) {
+        console.log("Error: ", data);
+      });
+  }
+})
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,12 @@
 $(document).ready(() => {
-    
-    $('#tweet-text').on('input', function() {
-      const value = 140 - $(this).val().length;
-      const counter = $(this).parent().find('#counterNumber');
-      counter.text(value);
-      if (value < 0) {
-        counter.addClass('red-font');
-      } else {
-        counter.removeClass('red-font');
-      }
-    });
-  });
-
-
   $('#tweet-text').on('input', function() {
     const value = 140 - $(this).val().length;
-    const counter  = $(this).parent().find("counterNumber");
+    const counter = $(this).parent().find('#counterNumber');
     counter.text(value);
     if (value < 0) {
-        counter.addClass('red-font');
+      counter.addClass('red-font');
     } else {
-        counter.removeClass('red-font');
+      counter.removeClass('red-font');
     }
-
   });
+});
